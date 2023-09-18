40cb96952588536de21386f33be171f4bc794dbf
katiemayjones <katiemayjones1990@gmail.com>
2023-09-18 08:48:04 +0100

    Added link to github rep

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <p>
+        <a
+          href="https://github.com/katiemayjones/react-weather-app"
+          target="_blank"
+        >
+          Open source code by Katiemay
+        </a>
+      </p>
     </div>
   );
 }
